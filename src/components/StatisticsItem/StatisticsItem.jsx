fb33b53919cff7762d3c78d1ac12f251f11ba8bd
Liudmyla_Taran <taran.lyudok@gmail.com>
2025-04-20 17:09:14 +0300

    task 1-3

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,12 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import style from './StatisticsItem.module.css';
+const StatisticsItem = ({ title, total, icon }) => {
+  return (
+    <>
+      <div>{icon}</div>
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
 
 export default StatisticsItem;
